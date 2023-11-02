d8693500f1532aced6f152b408fac860673c7031
Mateusz Śliwiński <mateuszsliwinski9@gmail.com>
2023-11-02 17:28:03 +0100

    add new module and register currency_app and rest_framework

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'currency_app.apps.CurrencyAppConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
