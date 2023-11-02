0966da3da3aeb80fa6b33699a08e4d350966263f
Mateusz Śliwiński <mateuszsliwinski9@gmail.com>
2023-11-02 23:57:21 +0100

    registration of models for the administrator

--- currency_app/admin.py
+++ currency_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from currency_app.models import Currency, ExchangeRate
+
+
 # Register your models here.
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):  # noqa D101
+    pass
+
+
+@admin.register(ExchangeRate)
+class ExchangeRateAdmin(admin.ModelAdmin):  # noqa D101
+    pass
